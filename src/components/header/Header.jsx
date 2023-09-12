5972b66a05f19873e6248fb9bc5d1523ddf02164
Foyez-Ahamed <foyez1435@gmail.com>
2023-09-12 15:40:05 +0600

    project setup and data loaded added

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold text-center py-10'>Knowledge cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
